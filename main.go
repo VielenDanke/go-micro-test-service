f1cc8315c308c7b7368d8f153410e66c10c53590
Vladislav Dankevich <vielendanke1991@gmail.com>
2021-01-16 22:31:48 +0600

    gprc service started

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"reflect"
 	"strings"
+	"syscall"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	jsoncodec "github.com/unistack-org/micro-codec-json"
 	fileconfig "github.com/unistack-org/micro-config-file"
@@ -20,6 +24,18 @@ import (
 	"github.com/vielendanke/test-service/handler"
 	pb "github.com/vielendanke/test-service/proto"
 )
+
+type GrpcHandler struct {
+}
+
+func (*GrpcHandler) GetTest(ctx context.Context, req *pb.GetTestRequest, resp *pb.GetTestResponse) error {
+	resp.Result = "good job"
+	return nil
+}
+func (*GrpcHandler) PostTest(ctx context.Context, req *pb.PostTestRequest, resp *pb.PostTestResponse) error {
+	resp.Id = uuid.New().String()
+	return nil
+}
 
 func configureHandlerToEndpoints(router *mux.Router, handler interface{}, endpoints []*api.Endpoint) error {
 	for _, v := range endpoints {
@@ -38,67 +54,107 @@ func configureHandlerToEndpoints(router *mux.Router, handler interface{}, endpoi
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+	errCh := make(chan error, 1)
 
 	logger.DefaultLogger = logger.NewLogger(logger.WithLevel(logger.TraceLevel))
-	cfg := NewConfig("test-service", "1.0")
 
-	if err := config.Load(ctx,
-		config.NewConfig(
-			config.Struct(cfg),
-		),
-		config.NewConfig(
-			config.AllowFail(true),
-			config.Struct(cfg),
-			config.Codec(jsoncodec.NewCodec()),
-			fileconfig.Path("local.json"),
-		),
-	); err != nil {
-		logger.Fatalf("Error while loading config %v\n", err)
-	}
-	options := append([]micro.Option{},
-		micro.Server(httpsrv.NewServer()),
-		micro.Context(ctx),
-		micro.Name(cfg.Server.Name),
-		micro.Version(cfg.Server.Version),
-	)
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+		errCh <- fmt.Errorf("%s", <-sigChan)
+	}()
 
-	grpcsrv.NewServer()
-	svc := micro.NewService(options...)
+	go func() {
+		grpcServer := grpcsrv.NewServer(
+			server.Name("test-service-grpc"),
+			server.Version("latest"),
+			server.Address(":9094"),
+		)
+		grpcsrv.Reflection(true)
 
-	if err := svc.Init(); err != nil {
-		logger.Fatalf("Failed to init service, %v\n", err)
-	}
+		options := append([]micro.Option{},
+			micro.Context(ctx),
+			micro.Name("test-service-grpc"),
+			micro.Version("latest"),
+		)
+		srv := micro.NewService(options...)
 
-	if err := svc.Init(
-		micro.Server(httpsrv.NewServer(
-			server.Name(cfg.Server.Name),
-			server.Version(cfg.Server.Version),
-			server.Address(cfg.Server.Addr),
-			server.Codec("application/json", jsoncodec.NewCodec()),
-		)),
-	); err != nil {
-		logger.Fatalf("Failed to init service with server, %v\n", err)
-	}
-	router := mux.NewRouter()
+		if err := srv.Init(); err != nil {
+			errCh <- err
+		}
+		if err := srv.Init(
+			micro.Server(grpcServer),
+		); err != nil {
+			errCh <- err
+		}
+		if err := srv.Server().Handle(srv.Server().NewHandler(&GrpcHandler{})); err != nil {
+			errCh <- err
+		}
+		if err := srv.Run(); err != nil {
+			errCh <- err
+		}
+	}()
 
-	handler := &handler.Handler{Codec: jsoncodec.NewCodec()}
+	go func() {
+		cfg := NewConfig("test-service", "1.0")
 
-	endpoints := pb.NewTestServiceEndpoints()
+		if err := config.Load(ctx,
+			config.NewConfig(
+				config.Struct(cfg),
+			),
+			config.NewConfig(
+				config.AllowFail(true),
+				config.Struct(cfg),
+				config.Codec(jsoncodec.NewCodec()),
+				fileconfig.Path("local.json"),
+			),
+		); err != nil {
+			logger.Fatalf("Error while loading config %v\n", err)
+		}
+		options := append([]micro.Option{},
+			micro.Server(httpsrv.NewServer()),
+			micro.Context(ctx),
+			micro.Name(cfg.Server.Name),
+			micro.Version(cfg.Server.Version),
+		)
+		svc := micro.NewService(options...)
 
-	if err := configureHandlerToEndpoints(router, handler, endpoints); err != nil {
-		logger.Fatalf("Error while mapping functions to handler %v", err)
-	}
+		if err := svc.Init(); err != nil {
+			errCh <- err
+		}
 
-	router.NotFoundHandler = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		logger.Infof("Not found, %v\n", r)
-	})
-	router.MethodNotAllowedHandler = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		logger.Infof("Method not allowed, %v\n", r)
-	})
-	if err := svc.Server().Handle(svc.Server().NewHandler(router)); err != nil {
-		logger.Fatalf("Failed to set http handler: %v\n", err)
-	}
-	if err := svc.Run(); err != nil {
-		logger.Fatalf("Failed to run service: %v\n", err)
-	}
+		if err := svc.Init(
+			micro.Server(httpsrv.NewServer(
+				server.Name(cfg.Server.Name),
+				server.Version(cfg.Server.Version),
+				server.Address(cfg.Server.Addr),
+				server.Codec("application/json", jsoncodec.NewCodec()),
+			)),
+		); err != nil {
+			errCh <- err
+		}
+		router := mux.NewRouter()
+
+		handler := &handler.Handler{Codec: jsoncodec.NewCodec()}
+
+		endpoints := pb.NewTestServiceEndpoints()
+
+		if err := configureHandlerToEndpoints(router, handler, endpoints); err != nil {
+			errCh <- err
+		}
+
+		router.NotFoundHandler = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			logger.Infof("Not found, %v\n", r)
+		})
+		router.MethodNotAllowedHandler = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			logger.Infof("Method not allowed, %v\n", r)
+		})
+		if err := svc.Server().Handle(svc.Server().NewHandler(router)); err != nil {
+			errCh <- err
+		}
+		if err := svc.Run(); err != nil {
+			errCh <- err
+		}
+	}()
+	fmt.Printf("Terminated: %v\n", <-errCh)
 }
